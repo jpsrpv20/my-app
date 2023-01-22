c4a73ad17e33d53874fc4657e644797c5e1caf14
Simons Rondona, Jean Pierre <simonsrondon1@llnl.gov>
2023-01-21 21:08:09 -0800

    commenting out aswsExports

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import NavigationBar from './NavigationBar.js';
 import DaveForm from './DaveForm.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+//import awsExports from './aws-exports';
+//Amplify.configure(awsExports);
 
 //const myFirstElement = <h1>Hello React!</h1>;
 
